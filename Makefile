121a8e25e1c4c4086a56158b737457ef61e1d7b9
david <david@Alster.(none)>
2013-01-12 21:19:15 +0100

    Accept --norm and --test as input; using groups of args

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ yg: yg.cpp
 
 testyg: yg.cpp yg
 	./yg  --m 24 --r 1 --c 0.2 --h 1.6 --n 1.2 --f 38.3 ${ARGS}
+
+cross: yg.cpp
+	i586-mingw32msvc-g++ yg.cpp -o yg.exe -I/usr/local/include/eigen3/ -Xassembler "-L/home/david/Downloads/boost_1_52_0" -L/home/david/Downloads/boost_1_52_0/bin.v2/libs -lboost_program_options
--- yg.cpp
+++ yg.cpp
@@ -18,21 +18,22 @@ int main(int argc, char** argv) {
   MatrixXf A(6,6);
   VectorXf b(6);
   VectorXf x(6);
+  
+  double Mmw, MFe, perC, perH, perN, perF;
 
-  double Mmw=24.3; // Mg
-  double MFe=2; // M-Fe ratio
-  double perC=0.2;
-  double perH=1.6;
-  double perN=1.2;
-  double perF=38.3;
-  char* optarg;
+
   int verbose=0;
+  int norm=1;
 
   try {
-    po::options_description desc("Allowed options");
-    desc.add_options()
+    po::options_description general("General options");
+    general.add_options()
       ("help", "produce help message")
-      ("verbose", "verbose")
+      ("test", "Run with a test dataset")
+      ("verbose", "verbose");
+    
+    po::options_description essential("Essential parameters");
+    essential.add_options()
       ("m", po::value<double>(), "Molecular weight of M")
       ("r", po::value<double>(),"M-Fe Ratio")
       ("c", po::value<double>(),"Percentage of C")
@@ -40,33 +41,71 @@ int main(int argc, char** argv) {
       ("n", po::value<double>(),"Percentage of N")
       ("f", po::value<double>(),"Percentage of F")
       ;
+    
+    po::options_description optional("Optional");
+    optional.add_options()
+      ("noNorm", "If set, the sum of Fe is *NOT* normalised to 1");
+
+    po::options_description desc("Allowed options");
+    desc.add(general).add(essential).add(optional);
+
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
-    if(vm.count("help")) {
+    if(vm.count("help") || argc==1) {
       cout << desc << "\n";
       return 0;
     }
     if(vm.count("verbose")) {
       verbose=1;
     }
-    if(vm.count("m")) {
-      MFe=vm["m"].as<double>();
+    if(vm.count("test")) {
+      Mmw=24.3; // Mg
+      MFe=2; // M-Fe ratio
+      perC=0.2;
+      perH=1.6;
+      perN=1.2;
+      perF=38.3;
+    } else {
+      if(vm.count("m")) {
+	Mmw=vm["m"].as<double>();
+      } else {
+	cerr << "Use --m to set the molecular weight of m" << endl;
+	return 1;
+      }
+      if(vm.count("r")) {
+	MFe=vm["r"].as<double>();
+      } else {
+	cerr << "Use --r to set the ratio of M:Fe" << endl;
+	return 1;
+      }
+      if(vm.count("c")) {
+	perC=vm["c"].as<double>();
+      } else {
+	cerr << "Use --c to set the percentage of C (C%)" << endl;
+	return 1;
+      }
+      if(vm.count("h")) {
+	perH=vm["h"].as<double>();
+      } else {
+	cerr << "Use --h to set the percentage of H (H%)" << endl;
+	return 1;
+      }
+      if(vm.count("n")) {
+	perN=vm["n"].as<double>();
+      } else {
+	cerr << "Use --n to set the percentage of N (N%)" << endl;
+	return 1;
+      }
+      if(vm.count("f")) {
+	perF=vm["f"].as<double>();
+      } else {
+	cerr << "Use --f to set the percentage of F (F%)" << endl;
+	return 1;
+      }
     }
-    if(vm.count("r")) {
-      MFe=vm["r"].as<double>();
-    }
-    if(vm.count("c")) {
-      perC=vm["c"].as<double>();
-    }
-    if(vm.count("h")) {
-      perH=vm["h"].as<double>();
-    }
-    if(vm.count("n")) {
-      perN=vm["n"].as<double>();
-    }
-    if(vm.count("f")) {
-      perF=vm["f"].as<double>();
+    if(vm.count("noNorm")) {
+      norm=0;
     }
   } catch(exception& e) {
     cerr << "error: " << e.what() << "\n";
@@ -99,11 +138,15 @@ int main(int argc, char** argv) {
     cout<<"==================== Output ====================" << endl;
   }
   x=calculate(A,b);
+  if(norm) {
+    double fesum=x[0]+x[1]+x[2];
+    x = x / fesum;
+  }
   cout << "FeF3" << "\t" << x[0] << endl;
   cout << "Fe(OH)3" << "\t" << x[1] << endl;
   cout << "Fe(NO3)3" << "\t" << x[2] << endl;
-  cout << "MgF2" << "\t" << x[3] << endl;
-  cout << "Mg(OAc)2" << "\t" << x[4] << endl;
+  cout << "MF2" << "\t" << x[3] << endl;
+  cout << "M(OAc)2" << "\t" << x[4] << endl;
   cout << "H2O" << "\t" << x[5] << endl;
   return(0);
 }
